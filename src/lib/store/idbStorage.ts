e26f214ca6b17e605c75809f307f46a35216300a
1sen3 <2818634977@qq.com>
2026-04-02 00:46:34 +0800

    fix: 优化播放列表持久化逻辑，修复了播放列表歌曲过多时导致的内存泄漏和磁盘占用

--- src/lib/store/idbStorage.ts
+++ src/lib/store/idbStorage.ts
@@ -1,11 +1,17 @@
 import { get, set, del } from "idb-keyval";
 import { StateStorage } from "zustand/middleware";
 
+const lastSavedString: Record<string, string> = {};
+
 export const idbStorage: StateStorage = {
   getItem: async (name: string): Promise<string | null> => {
     return (await get(name)) ?? null;
   },
   setItem: async (name: string, value: string): Promise<void> => {
+    if (lastSavedString[name] === value) {
+      return;
+    }
+    lastSavedString[name] = value;
     await set(name, value);
   },
   removeItem: async (name: string): Promise<void> => {
